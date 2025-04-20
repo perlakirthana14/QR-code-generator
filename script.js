6dd6a63dc7839a96c890397936b995ecc28aea8d
perlakirthana14 <kirthanaperla@gmail.com>
2025-04-20 19:48:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+ function generateqr(){
+    let qrcode=document.getElementById("inputtag").value;
+    let goourl="https://www.google.com/search?tbm=isch&q="+qrcode
+    let res="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+encodeURIComponent(goourl);
+    document.getElementById("imgtag").src= res;//safely encode a URL by converting special characters 
+    // (like spaces, &, ?, =, etc.) into a format that browsers and APIs can understand correctly.
+}
+
+
